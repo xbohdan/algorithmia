08460a53738ce826df2a3473e0481dd075518475
cutieskye <68032402+cutieskye@users.noreply.github.com>
2021-02-05 01:02:10 +0000

    Find the greatest common divisor and least common multiple

---
+++ algorithmic-warmup/lcm.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+long long lcm_naive(int a, int b)
+{
+	for (long l = 1; l <= (long long)a * b; ++l)
+		if (l % a == 0 && l % b == 0)
+			return l;
+
+	return (long long)a * b;
+}
+
+int gcd_fast(int a, int b)
+{
+	if (b == 0)
+		return a;
+	return gcd_fast(b, a % b);
+}
+
+long long lcm_fast(int a, int b)
+{
+	if (a == 0 || b == 0)
+		return 0;
+	int gcd = gcd_fast(a, b);
+	return static_cast<long long>(a / gcd) * b;
+}
+
+int main()
+{
+	int a, b;
+	std::cin >> a >> b;
+	std::cout << lcm_fast(a, b) << std::endl;
+	return 0;
+}
